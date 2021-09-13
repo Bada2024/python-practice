7bd61cd47c700dcef2d27070e62eaa9a273f516f
Bada2024 <830561@mystma.org>
2021-09-13 09:39:22 -0500

    top 5 movies

---
+++ main.py
@@ -0,0 +1,14 @@
+#print 1st movie
+print("Toy story")
+
+#print 2nd movie
+print("toy story 2")
+
+#print 3rd movie
+print("inside out")
+
+#print 4th movie
+print("cars")
+
+#print 5th movie
+print("cars 2")
