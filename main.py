a7ef5fb49da59111cc417150cd85228a08fbe825
Bada2024 <830561@mystma.org>
2021-09-20 08:51:35 -0500

    madlib fixed  yo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #def madlib 
 def madlib(a,b,c,d,e,f,g,h,j):
     #a adjective,b  noun,c adjective,d noun/place,e adjective,f adjective,g noun/vehicle,h adjective,j adjective
-    print("jump,phone,playing,walmart,throwing,eating,motorcycle,wearing,reading")
-print("Star Wars is a"+ madlib(a,b)+"of"+madlib(c)+"versus evil in a"+ madlib(d)+)
+    print("Star Wars is a "+ a + b + " of" + " versus evil in a "+ c + " far far away There are " + d + " battles between " + e + f +" in "+ g + " space and "+ h + " duels with " + j)
+
+madlib(" great"," phone"," annyoing"," walmart"," funny"," rude"," motorcycle"," amazing"," lucky")
 
 
 
